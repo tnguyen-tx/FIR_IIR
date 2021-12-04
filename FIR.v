f538bde829d8669c07e4e6c813c28c61e504ae58
Thuy Nguyen <57820377+tnguyen-tx@users.noreply.github.com>
2021-12-04 11:34:44 -0600

    Update FIR.v

--- FIR.v
+++ FIR.v
@@ -1,5 +1,6 @@
 `timescale 1ns / 1ps
 `include "multiply.v"
+
 module FIR(
 clk,
 rst,
@@ -13,28 +14,26 @@ input[7:0] a,b,c;
 input[7:0] x;
 output[7:0] y;
 
-reg[7:0] x_n2;
-reg[7:0] x_n1;
-	wire [7:0] mul_xn2;
-	wire [7:0] mul_xn1;
-	wire [7:0] mul_xn0;
+reg[7:0] x1;
+reg[7:0] x2;
 
-//your code here=============
-multiply mul_x0(.a(x),.b(a),.c(mul_xn0));
-multiply mul_x1(.a(x_n1),.b(b),.c(mul_xn1));
-multiply mul_x2(.a(x_n2),.b(c),.c(mul_xn2));
+wire[7:0] mula_xn, mulb_xn1, mulc_xn2;
 
-always @(posedge clk or posedge rst) begin
-	if (rst) begin
-		x_n1 <= 0;
-		x_n2 <= 0;
-	end
-	else begin
-		x_n2 <= x_n1;
-		x_n1 <= x;
-	end
+multiply mul0 (.a(a), .b(x), .c(mula_xn));
+multiply mul1 (.a(b), .b(x1), .c(mulb_xn1));
+multiply mul2 (.a(c), .b(x2), .c(mulc_xn2));
+
+always @(posedge clk or negedge rst) begin
+    if (!rst) begin
+        x1 <=0;
+        x2 <=0;
+    end
+    else begin
+        x1 <= x;
+        x2 <= x1;
+    end
 end
-//your code here=============
-assign y = mul_xn0+mul_xn1+mul_xn2;
 
-endmodule
+assign y = mula_xn + mulb_xn1 + mulc_xn2;
+
+endmodule //FIR
