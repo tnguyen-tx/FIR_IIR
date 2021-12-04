756011a35de0f379ae045841de99bd4660ddeb32
Thuy Nguyen <57820377+tnguyen-tx@users.noreply.github.com>
2021-12-04 14:35:16 -0600

    Update IIR.v

--- IIR.v
+++ IIR.v
@@ -23,14 +23,13 @@ wire [7:0] m_x1;
 wire [7:0] m_y1;
 wire [7:0] m_y2;
 
-//your code here=============
 	multiply mx(.a(x), .b(a), .c(m_x));
 	multiply mx1(.a(x_n1), .b(b), .c(m_x1));
 	multiply my1(.a(y_n1), .b(c), .c(m_y1));
 	multiply my2(.a(y_n2), .b(d), .c(m_y2));
 
-	always @(posedge clk or posedge rst) begin
-		if (rst) begin 
+	always @(posedge clk or negedge rst) begin
+		if (!rst) begin 
 			x_n1 <=0;
 			y_n1 <=0;
 			y_n2 <=0;
@@ -43,6 +42,6 @@ wire [7:0] m_y2;
 	end
 
 assign y =	m_x + m_x1 + m_y1 + m_y2;
-//your code here=============
+
 
 endmodule
